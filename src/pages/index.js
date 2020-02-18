59c752d152081fef9c4859b2e9db4dfb4434d087
Vilim <vilim.krajinovic@gmail.com>
2020-02-18 22:52:06 +0100

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,9 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import styled from "styled-components";
+
+const Container = styled.div;
 
 const IndexPage = () => (
   <Layout>
